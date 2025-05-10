15c75132545c86b3f123bd7923acc972bb4421ea
chaharhimanshu1004 <chaharhimanshu03@gmail.com>
2025-05-11 00:19:28 +0530

    removed logs

--- app/api/get-video-script/route.js
+++ app/api/get-video-script/route.js
@@ -4,13 +4,10 @@ import { NextResponse } from "next/server";
 export async function POST(req) {
     try{
         const { prompt } = await req.json();
-        console.log('>>>>prompt', prompt);
 
         const result = await chatSession.sendMessage(prompt);
-        console.log('>>>>response from getting video script',result);
 
         const responseText = await result.response.text();
-        console.log('>>>>responseText',responseText);
 
         return NextResponse.json({
             result:JSON.parse(responseText)
