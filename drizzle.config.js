474a71a379e005e215f49d484abe1be94d0acaac
chaharhimanshu1004 <chaharhimanshu03@gmail.com>
2025-05-10 19:43:25 +0530

    fix: publicly exposed db url

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default defineConfig({
   dialect: "postgresql",
   schema: "./configs/schema.js",
   dbCredentials: {
-    url: 'postgresql://shortslab-db_owner:0UIQSXui5qeM@ep-sweet-flower-a1o7qe3h.ap-southeast-1.aws.neon.tech/shortslab-db?sslmode=require'
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DATABASE_URL
   }
 });
