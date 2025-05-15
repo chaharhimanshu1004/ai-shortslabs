ce3ce9b9d95ae97f92024688000a9efb0c8d5223
chaharhimanshu1004 <chaharhimanshu03@gmail.com>
2025-05-16 00:50:27 +0530

    enable captions

--- app/dashboard/_components/RemotionVideo.jsx
+++ app/dashboard/_components/RemotionVideo.jsx
@@ -1,33 +1,60 @@
 import React from 'react'
-import { AbsoluteFill, Img, Sequence, useVideoConfig } from 'remotion'
+import { AbsoluteFill, Img, Sequence, useVideoConfig, Audio, useCurrentFrame, staticFile } from 'remotion'
 
 function RemotionVideo({ script, audioFileUrl, captions, imageList, setDurationInFrames }) {
 
   const { fps } = useVideoConfig();
-
+  const frame = useCurrentFrame();
   const getDurationFrame = () => {
     const duration = captions[captions.length - 1]?.end / 1000 * fps
     setDurationInFrames(duration)
     return duration ;
   }
 
+  const getCurrentCaption = () => {
+    const currentTime = (frame / 30) * 1000; // 30 = fps
+    const currentCaption = captions.find((caption) => {
+      return currentTime >= caption.start && currentTime <= caption.end;
+    });
+    return currentCaption?.text || '';
+  }
+
 
   return (
     <AbsoluteFill className='bg-black'>
       {
-        imageList?.map((item, index) => (
-          <>
-            <Sequence key={index} from={((index * getDurationFrame()) / imageList?.length)} durationInFrames={getDurationFrame()}>
-              <Img src={item} style={{
-                width: '100%',
-                height: '100%',
-                objectFit: 'cover',
-              }} >
-              </Img>
-            </Sequence>
-          </>
-        ))
+        imageList?.map((item, index) => {
+          const startTime = ((index * getDurationFrame()) / imageList?.length);
+          return (
+            <>
+              <Sequence key={index} from={startTime} durationInFrames={getDurationFrame()}>
+                <AbsoluteFill style={{ justifyContent: 'center', alignItems: 'center' }}>
+                  <Img src={item} style={{
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'cover',
+                  }} />
+                  <AbsoluteFill style={{
+                    color: 'white',
+                    justifyContent: 'center',
+                    top: undefined,
+                    bottom: 50,
+                    height: 150,
+                    textAlign: 'center',
+                    width: '100%',
+                  }}>
+                    <h2 className='text-2xl'>{getCurrentCaption()}</h2>
+                  </AbsoluteFill>
+                </AbsoluteFill>
+              </Sequence>
+            </>
+          )
+        })
       }
+      <AbsoluteFill>
+        {audioFileUrl && <Audio src={audioFileUrl} startFrom={0} />}
+      </AbsoluteFill>
+
 
     </AbsoluteFill>
   )
