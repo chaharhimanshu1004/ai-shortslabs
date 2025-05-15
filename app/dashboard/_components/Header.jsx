4032a0479cdef22f3c6817d1ffc83d108a339b64
chaharhimanshu1004 <chaharhimanshu03@gmail.com>
2025-05-16 02:41:23 +0530

    user detail for credis

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import { UserButton } from '@clerk/nextjs'
+import { UserDetailContext } from '@/app/_context/UserDetailContext'
+
+
 const Header = () => {
+
+    const { userDetail, setUserDetail } = useContext(UserDetailContext);
+
     return (
         <div className='flex p-3 px-5 items-center justify-between shadow-md'>
             <div className='flex gap-3 items-center'>
@@ -10,6 +16,12 @@ const Header = () => {
                 <h2 className='font-bold text-xl'>Ai shorts lab</h2>
             </div>
             <div className='flex gap-3 items-center'>
+                <div className='flex gap-1 items-center'>
+                    <Image src={'/coin.png'} width={20} height={20} />
+                    <h2>
+                        {userDetail?.credits}
+                    </h2>
+                </div>
                 <Button>
                     Dashboard
                 </Button>
