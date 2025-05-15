69c0c746c039feda500998ece2162b0dea5306a1
chaharhimanshu1004 <chaharhimanshu03@gmail.com>
2025-05-16 01:17:56 +0530

    getVideoList fun

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,12 +1,28 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { CirclePlus } from 'lucide-react';
 import EmptyState from './_components/EmptyState';
 import Link from 'next/link';
+import { VideoData } from '@/configs/schema';
+import { db } from '@/configs/db';
+import { eq } from 'drizzle-orm';
+import { useUser } from '@clerk/nextjs';
 
 const Dashboard = () => {
   const [videoList, setVideoList] = useState([]);
+  const { user } = useUser();
+  const getVideoList = async () => {
+    const result = await db.select().from(VideoData).where(eq(VideoData?.createdBy, user?.primaryEmailAddress?.emailAddress));
+    setVideoList(result);
+  }
+
+  useEffect(() => {
+    if(user){
+      getVideoList();
+    }
+  }, [user])
+  
   return (
     <div>
       <div className='flex justify-between items-center'>
